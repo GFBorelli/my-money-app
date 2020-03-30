b322ef44f06c79daefd954ca71678aad69dfe718
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-03-30 16:06:50 -0300

    database counter and error treatment added

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 
 module.exports = mongoose.connect('mongodb://localhost/moneymanager', { useUnifiedTopology: true, useNewUrlParser: true })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
