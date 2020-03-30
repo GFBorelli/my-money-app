68bbeb13c79301ea673ca78233b76412aa89bb6a
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-03-30 17:59:45 -0300

    billing summary - route and aggregation added

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -7,9 +7,25 @@ BillingCycle.updateOptions({ new: true, runValidators: true })
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
         if (error) {
-            res.status(500).json({ errors: [errors] })
+            res.status(500).json({ errors: [error] })
         } else {
             res.json({ value })
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{
+        $project: { creditSum: { $sum: '$credits.value' }, debitSum: { $sum: '$debits.value' } }
+    }, {
+        $group: { _id: null, creditTotalSum: { $sum: '$creditSum' }, debitTotalSum: { $sum: '$debitSum' } }
+    }, {
+        $project: { _id: 0, creditTotalSum: 1, debitTotalSum: 1 }
+    }], (error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json(result[0] || { credit: 0, debit: 0 })
         }
     })
 })
