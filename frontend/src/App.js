c336a637fe9c42eb56bd59ebd18f1ecb6668c56a
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-03-31 15:15:23 -0300

    Header added
    Still need to edit the nav, dropdowns, etc, just adding a header to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './common/template/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
     </div>
   );
 }
