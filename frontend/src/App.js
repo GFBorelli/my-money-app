55536c0082d45d8ba695980913c7151653b14424
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-03-31 16:16:06 -0300

    Sidebar added
    Sidebar added, will receive future implementations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './common/template/Header'
+import Sidebar from './common/template/Sidebar'
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
+      <Sidebar />
     </div>
   );
 }
