414cfc98f6f729824dd9ecb450363f730a125c66
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-04-02 15:00:22 -0300

    Component TabContent Added
    Component TabContent added and integrated with Redux, will receive improvements

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { selectTab } from '../common/tabs/tabsActions'
 
 import PageHeader from '../common/template/PageHeader'
 import Tabs from '../common/tabs/Tabs'
+import TabContent from '../common/tabs/TabContent'
 
 class BillingCycle extends Component {
+
+    componentDidMount() {
+        this.props.selectTab('list')
+    }
 
     render() {
         return (
             <div>
                 <PageHeader name='Ciclos de Pagamentos' />
                 <Tabs />
+                <TabContent id='list'><h1>listar</h1></TabContent>
+                <TabContent id='add'><h1>add</h1></TabContent>
+                <TabContent id='edit'><h1>editar</h1></TabContent>
+                <TabContent id='remove'><h1>remover</h1></TabContent>
             </div>
         )
     }
 }
 
-export default BillingCycle
+const mapStateToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+export default connect(null, mapStateToProps)(BillingCycle)
---
+++ frontend/src/common/tabs/TabContent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+// import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+class TabContent extends Component {
+    render() {
+        const selected = this.props.tabs.selected === this.props.id
+        return (
+            <div id={this.props.id} className={selected ? '' : 'text-hide'}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({ tabs: state.tabs })
+export default connect(mapStateToProps)(TabContent)
